fa7e2968600cb334e05388af3c6acfc1bdf1bfea
Ryan How <ryanhow1999@yahoo.ca>
2020-03-07 01:48:22 -0500

    step model complete

---
+++ models/Step.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const Recipe = require('./Recipe');
+const db = require('./index');
+
+const Step = db.sequelize.define('step', {
+    step_no: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
+    recipe_id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        onDelete: '', //@TODO
+        references: {
+            model: Recipe,
+            key: 'recipe_id',
+            as: 'recipe_id'
+        }
+    },
+    description: {
+        allowNull: false,
+        type: Sequelize.STRING
+    }
+});
+
+module.exports = Step;
