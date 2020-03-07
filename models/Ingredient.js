c973666008a12f698942caf2ad7e7c8b8fae14c8
Ryan How <ryanhow1999@yahoo.ca>
2020-03-07 01:47:58 -0500

    ingredient model complete

---
+++ models/Ingredient.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const Recipe = require('./Recipe');
+const db = require('./index');
+
+const Ingredient = db.sequelize.define('ingredient', {
+    ingredient_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+    },
+    recipe_id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        onDelete: '', //@TODO
+        references: {
+            model: Recipe,
+            key: 'recipe_id',
+            as: 'recipe_id'
+        }
+    },
+    name: {
+        allowNull: false,
+        type: Sequelize.STRING
+    },
+    quantity: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+    },
+    unit: { //Necessary?
+        allowNull: false,
+        type: Sequelize.STRING
+    }
+});
+
+module.exports = Ingredient;
