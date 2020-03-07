a0d88c095f578db62273dc34c7af55a7797c8fdc
Ryan How <ryanhow1999@yahoo.ca>
2020-03-07 01:47:28 -0500

    recipe model complete

---
+++ models/Recipe.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const db = require('./index');
+
+const Recipe = db.sequelize.define('recipe', {
+    recipe_id:{
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+    },
+    title: {
+        allowNull: false,
+        type: Sequelize.STRING
+    },
+    rating: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+    },
+    fat: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+    },
+    calories: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+    },
+    protein: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+    },
+    sodium: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+    }
+});
+
+module.exports = Recipe;
