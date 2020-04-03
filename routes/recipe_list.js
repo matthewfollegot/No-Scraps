e3f6e0fe9024a1bfa5c0cbd895a5c280126beaf2
Ryan How <ryanhow1999@yahoo.ca>
2020-04-03 05:26:14 -0400

    partially completed recipe page

--- routes/recipe_list.js
+++ routes/recipe_list.js
@@ -4,10 +4,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
     try {
        res.render('recipe_list');
-        // res.render('recipe_list', {data:{userQuery:req.params.userQuery}});
     }
     catch(err) {
-        res.send({message: "Failed tossssssssssssss retrive recipes based on inputted ingredients", error: err});
+        res.send({message: "Failed to retrive recipe page", error: err});
     }
 });
 
